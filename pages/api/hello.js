8748d61d2ee6607425d2943f53fe599278b03d0b
Md.Fahad Ali <fahad288ali@gmail.com>
2023-08-10 19:03:00 +0600

    update :tada:

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,13 +1,10 @@
 import handler from "./handler";
-import db from "./db";
+
 
 handler.get(async function (req, res) {
-  const result = await db.select().from("auth");
   
-  // console.log(req.csrfToken());
-  // console.log(process.env.CSRF_SECRET);
-
-  res.json(result);
+  
+  res.json(process.env);
 });
 
 export default handler;
